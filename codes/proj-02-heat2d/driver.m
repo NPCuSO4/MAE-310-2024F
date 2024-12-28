ba51c0ea37b3a9ced204dceb83295203e70d73bb
NPCuSO4 <NPCuSO4>
2024-12-28 19:31:44 +0800

    Finished the error term of 2D code

--- codes/proj-02-heat2d/driver.m
+++ codes/proj-02-heat2d/driver.m
@@ -208,4 +208,77 @@ end
 % HEAT.mat
 save("HEAT", "disp", "n_el_x", "n_el_y");
 
+% Calculate relative error
+E_0 = 0.0; U_0 = 0.0;
+E_1 = 0.0; U_1 = 0.0;
+
+% quadrature loop
+for ee = 1 : n_el
+  x_ele = x_coor( IEN(ee, 1:n_en) );
+  y_ele = y_coor( IEN(ee, 1:n_en) );
+  u_ele =   disp( IEN(ee, 1:n_en) );
+  
+  if mesh_type == 1
+    % set quadrature points for error term
+    n_int_xi  = 5;
+    n_int_eta = 5;
+    n_int     = n_int_xi * n_int_eta;
+    [xi, eta, weight] = Gauss2D(n_int_xi, n_int_eta);
+    for ll = 1 : n_int
+      x_l = 0.0; y_l = 0.0; u_l = 0.0;
+      dx_dxi = 0.0; dx_deta = 0.0;
+      dy_dxi = 0.0; dy_deta = 0.0;
+      du_dxi = 0.0; du_deta = 0.0;
+      for aa = 1 : n_en
+        x_l = x_l + x_ele(aa) * Quad(aa, xi(ll), eta(ll));
+        y_l = y_l + y_ele(aa) * Quad(aa, xi(ll), eta(ll));
+        u_l = u_l + u_ele(aa) * Quad(aa, xi(ll), eta(ll));
+        [Na_xi, Na_eta] = Quad_grad(aa, xi(ll), eta(ll));
+        dx_dxi  = dx_dxi  + x_ele(aa) * Na_xi;
+        dx_deta = dx_deta + x_ele(aa) * Na_eta;
+        dy_dxi  = dy_dxi  + y_ele(aa) * Na_xi;
+        dy_deta = dy_deta + y_ele(aa) * Na_eta;
+        du_dxi  = du_dxi  + u_ele(aa) * Na_xi;
+        du_deta = du_deta + u_ele(aa) * Na_eta;
+      end
+      detJ = dx_dxi * dy_deta - dx_deta * dy_dxi;
+      E_0 = E_0 + weight(ll) * (u_l - exact(x_l, y_l))^2 * detJ;
+      U_0 = U_0 + weight(ll) * exact(x_l, y_l)^2 * detJ;
+      E_1 = E_1 + weight(ll) * ((du_dxi/dx_dxi - exact_x(x_l, y_l) )^2 + (du_deta/dy_deta - exact_y(x_l, y_l) )^2) * detJ;
+      U_1 = U_1 + weight(ll) * (exact_x(x_l, y_l)^2 + exact_y(x_l, y_l)^2) * detJ;
+    end
+  else
+    n_int = 4;
+    [xi, eta, weight] = GaussTri(n_int);
+    for ll = 1 : n_int
+      x_l = 0.0; y_l = 0.0; u_l = 0.0;
+      dx_dxi = 0.0; dx_deta = 0.0;
+      dy_dxi = 0.0; dy_deta = 0.0;
+      du_dxi = 0.0; du_deta = 0.0;
+      for aa = 1 : n_en
+        x_l = x_l + x_ele(aa) * Tri(aa, xi(ll), eta(ll));
+        y_l = y_l + y_ele(aa) * Tri(aa, xi(ll), eta(ll));
+        u_l = u_l + u_ele(aa) * Tri(aa, xi(ll), eta(ll));
+        [Na_xi, Na_eta] = Tri_grad(aa, xi(ll), eta(ll));
+        dx_dxi  = dx_dxi  + x_ele(aa) * Na_xi;
+        dx_deta = dx_deta + x_ele(aa) * Na_eta;
+        dy_dxi  = dy_dxi  + y_ele(aa) * Na_xi;
+        dy_deta = dy_deta + y_ele(aa) * Na_eta;
+        du_dxi  = du_dxi  + u_ele(aa) * Na_xi;
+        du_deta = du_deta + u_ele(aa) * Na_eta;
+      end
+      detJ = dx_dxi * dy_deta - dx_deta * dy_dxi;
+      E_0 = E_0 + weight(ll) * (u_l - exact(x_l, y_l))^2 * detJ;
+      U_0 = U_0 + weight(ll) * exact(x_l, y_l)^2 * detJ;
+      E_1 = E_1 + weight(ll) * ((du_dxi/dx_dxi - exact_x(x_l, y_l) )^2 + (du_deta/dy_deta - exact_y(x_l, y_l) )^2) * detJ;
+      U_1 = U_1 + weight(ll) * (exact_x(x_l, y_l)^2 + exact_y(x_l, y_l)^2) * detJ;
+    end
+  end
+end
+E_0 = E_0^0.5; U_0 = U_0^0.5;
+E_1 = E_1^0.5; U_1 = U_1^0.5;
+E_L2 = E_0 / U_0
+E_H1 = E_1 / U_1
+
+
 % EOF
