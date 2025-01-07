e8c7eb38ece784fbe20aa578b1a7bc2e00371f33
NPCuSO4 <NPCuSO4>
2025-01-08 02:16:30 +0800

    Set up the structure of final project

---
+++ FinalProject/main.m
@@ -0,0 +1,21 @@
+% Set up the problem (para & boundary etc.)
+
+
+
+% Read the mesh (IEN)
+
+% Set the boundary (ID)
+
+
+
+% Assemble K & F
+
+% Get disp
+
+
+
+% Process stress & strain
+
+% Plots
+
+% Error terms
